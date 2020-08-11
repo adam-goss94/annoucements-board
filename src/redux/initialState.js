985f4c68a832698cfc5a832e40ac2df420b506d4
Adam Goss <adam.goss94@gmail.com>
2020-08-11 22:32:02 +0200

    Add posts to InitialState

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -1,9 +1,23 @@
 export const initialState = {
   posts: {
-    data: {},
-    loading: {
-      active: false,
-      error: false,
+    data: [{
+      id: '1',
+      title: 'Lorem Ipsum',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      email: 'ada@wp.pl',
     },
+    {
+      id: '2',
+      title: 'Lorem Ipsum',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+      email: 'kasia@wp.pl',
+    }],
   },
+
+  loading: {
+    active: false,
+    error: false,
+  },
+
+  userLogged: true,
 };
