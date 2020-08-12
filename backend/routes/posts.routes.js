f42cb5820d71a54c5961fb71c0714167125ddce2
Adam Goss <adam.goss94@gmail.com>
2020-08-12 08:39:13 +0200

    Add backend files and new endpoints

---
+++ backend/routes/posts.routes.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const router = express.Router();
+
+const Post = require('../models/post.model');
+
+router.get('/posts', async (req, res) => {
+  try {
+    const result = await Post
+      .find({status: 'published' || ''});
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/posts', async (req, res) => {
+  try {
+    console.log('saving:', req.body);
+    const post = new Post({...req.body, status: 'published' });
+    const result = await post.save();
+
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const result = await Post
+      .findById(req.params.id);
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/myposts', async (req, res) => {
+  try {
+    const result = await Post
+      .find({author: 'adam.goss94@gmail.com'});
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+
+module.exports = router;
